27dc61592fc88dd7086028cefa1da4427120c204
annatsai <annatsaianna@gmail.com>
2023-11-26 17:29:19 +0800

    list

---
+++ list.py
@@ -0,0 +1,10 @@
+# list 清單
+a = ['totyta', 'honda']#可以混裝不同類型,索引totyta=0,honda=1
+print(a)
+print(a[0])#列印第1個
+print(a[1])#列印第2個
+a.append('Audi')#把Audi加入清單
+print(a)
+print(len(a))#計算清單長度，答案應為3
+print('totyta' in a)#檢查totyta有沒有在清單裡
+print('Audi' in a)#檢查Audi有沒有在清單裡,是非題,in 後面一定要加清單
